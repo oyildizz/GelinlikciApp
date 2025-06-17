7e524837162b74b3ea4d66e057fd8b6c31fef95c
oyildizz <oyildizz@icloud.com>
2025-06-17 19:11:45 +0300

    .

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -105,7 +105,7 @@ export default function Layout() {
       />
       <Tab.Screen name="Bize Sor" component={AskUsModal} />
       <Tab.Screen
-        name="Prova Kartı"
+        name="Prova Oluştur"
         component={AppointmentCardScreen}
         listeners={({ navigation }) => ({
           tabPress: () => {
